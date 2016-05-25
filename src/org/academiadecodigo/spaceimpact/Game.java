dd3089e6b47e7c6cf54245b441ce67c58f9caeb0
Tomás Ferreira <tomasferreira@gmail.com>
2016-05-25 20:54:21 +0100

    working on game

--- src/org/academiadecodigo/spaceimpact/Game.java
+++ src/org/academiadecodigo/spaceimpact/Game.java
@@ -1,8 +1,6 @@
 package org.academiadecodigo.spaceimpact;
 
-import org.academiadecodigo.spaceimpact.gameobjects.EnemyShip;
-import org.academiadecodigo.spaceimpact.gameobjects.GameObjectType;
-import org.academiadecodigo.spaceimpact.gameobjects.SpaceShipFactory;
+import org.academiadecodigo.spaceimpact.gameobjects.*;
 import org.academiadecodigo.spaceimpact.representable.Background;
 import org.academiadecodigo.spaceimpact.simplegfx.SimpleGfxBackground;
 import org.academiadecodigo.spaceimpact.simplegfx.SimpleGfxRepresentableFactory;
@@ -17,23 +15,54 @@ public class Game {
     public final int SIZE_WIDTH = 100;
     public final int SIZE_HEIGHT = 100;
     private final int STARTING_ENEMY_SHIPS = 10;
+    private final int DELAY = 50;
 
-//    public void init(){
-//        Background background = new SimpleGfxBackground();
-//        background.init(SIZE_WIDTH, SIZE_HEIGHT);
-//
-//        SpaceShipFactory spaceShipFactory = new SpaceShipFactory(new SimpleGfxRepresentableFactory());
-//
-//        LinkedList<EnemyShip> enemyShips = new LinkedList<>();
-//
-//        for (int i = 0; i < STARTING_ENEMY_SHIPS; i++) {
-//            enemyShips.add(spaceShipFactory.createObject(GameObjectType.ENEMYSHIP, ))
-//        }
-//
-//        for (enemyShips enemyShip: enemyShips) {
-//
-//        }
-//
-//
-//    }
+    private int enemyStartingPosX; //por todos na mesma posiçao X mas variar a posiçao Y
+    private int enemyStartingPosY;
+
+    private int playerStartingPosX;
+    private int playerStartingPosY;
+
+    private Background background;
+    private SpaceShipFactory spaceShipFactory;
+    private Spaceship playerShip;
+    private LinkedList<EnemyShip> enemyShips;
+
+    public Game() {
+    }
+
+    public void init() {
+
+        background = new SimpleGfxBackground();
+
+        background.init(SIZE_WIDTH, SIZE_HEIGHT);
+        
+        spaceShipFactory = new SpaceShipFactory(new SimpleGfxRepresentableFactory());
+
+        playerShip = (PlayerShip) spaceShipFactory.createObject(GameObjectType.PLAYERSHIP, playerStartingPosX, playerStartingPosY);
+
+        enemyShips = new LinkedList<>();
+        for (int i = 0; i < STARTING_ENEMY_SHIPS; i++) {
+            enemyShips.add((EnemyShip) spaceShipFactory.createObject(GameObjectType.ENEMYSHIP, enemyStartingPosX, enemyStartingPosY));
+        }
+    }
+
+    public void start() throws InterruptedException {
+
+        while(!playerShip.isDestroyed()){
+
+            Thread.sleep(DELAY);
+            move();
+        }
+
+
+    }
+
+    public void move(){
+        for (int i = 0; i < enemyShips.size(); i++) {
+            enemyShips.get(i).move();
+            playerShip.move();
+
+        }
+    }
 }
