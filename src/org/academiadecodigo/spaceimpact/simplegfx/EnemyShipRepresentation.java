ce7ebb333a92bd3bc450a1adcd4eb41fe3ce9313
Tomás Ferreira <tomasferreira@gmail.com>
2016-05-25 14:57:38 +0100

    added representation classes on simplegfx package

---
+++ src/org/academiadecodigo/spaceimpact/simplegfx/EnemyShipRepresentation.java
@@ -0,0 +1,24 @@
+package org.academiadecodigo.spaceimpact.simplegfx;
+
+import org.academiadecodigo.spaceimpact.representable.Representable;
+
+/**
+ * Created by codecadet on 25/05/16.
+ */
+public class EnemyShipRepresentation implements Representable {
+
+    @Override
+    public int getX() {
+        return 0;
+    }
+
+    @Override
+    public int getY() {
+        return 0;
+    }
+
+    @Override
+    public void move(int dx, int dy) {
+
+    }
+}
