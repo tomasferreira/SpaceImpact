3691d536e6ba0c37e1b861f74d76aa891b0e62c3
Tomás Ferreira <tomasferreira@gmail.com>
2016-05-25 18:40:58 +0100

    added test class

--- src/org/academiadecodigo/spaceimpact/Game.java
+++ src/org/academiadecodigo/spaceimpact/Game.java
@@ -1,7 +1,13 @@
 package org.academiadecodigo.spaceimpact;
 
+import org.academiadecodigo.spaceimpact.gameobjects.EnemyShip;
+import org.academiadecodigo.spaceimpact.gameobjects.GameObjectType;
+import org.academiadecodigo.spaceimpact.gameobjects.SpaceShipFactory;
 import org.academiadecodigo.spaceimpact.representable.Background;
 import org.academiadecodigo.spaceimpact.simplegfx.SimpleGfxBackground;
+import org.academiadecodigo.spaceimpact.simplegfx.SimpleGfxRepresentableFactory;
+
+import java.util.LinkedList;
 
 /**
  * Created by codecadet on 23/05/16.
@@ -10,8 +16,24 @@ public class Game {
 
     public final int SIZE_WIDTH = 100;
     public final int SIZE_HEIGHT = 100;
+    private final int STARTING_ENEMY_SHIPS = 10;
 
-    public void init(){
-        Background background = new SimpleGfxBackground();
-    }
+//    public void init(){
+//        Background background = new SimpleGfxBackground();
+//        background.init(SIZE_WIDTH, SIZE_HEIGHT);
+//
+//        SpaceShipFactory spaceShipFactory = new SpaceShipFactory(new SimpleGfxRepresentableFactory());
+//
+//        LinkedList<EnemyShip> enemyShips = new LinkedList<>();
+//
+//        for (int i = 0; i < STARTING_ENEMY_SHIPS; i++) {
+//            enemyShips.add(spaceShipFactory.createObject(GameObjectType.ENEMYSHIP, ))
+//        }
+//
+//        for (enemyShips enemyShip: enemyShips) {
+//
+//        }
+//
+//
+//    }
 }
---
+++ src/org/academiadecodigo/spaceimpact/Test.java
@@ -0,0 +1,11 @@
+package org.academiadecodigo.spaceimpact;
+
+/**
+ * Created by codecadet on 25/05/16.
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        //TEST STUFF HERE
+    }
+}
