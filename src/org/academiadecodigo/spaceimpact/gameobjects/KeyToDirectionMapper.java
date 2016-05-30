5021284282b260920866fa9a8f7dd5984b6d89dd
skanz <rodolfocmattos@gmail.com>
2016-05-30 18:04:27 +0100

    KeyToDirectionMapper added

--- src/org/academiadecodigo/spaceimpact/gameobjects/KeyToDirectionMapper.java
+++ src/org/academiadecodigo/spaceimpact/gameobjects/KeyToDirectionMapper.java
@@ -1,10 +1,32 @@
 package org.academiadecodigo.spaceimpact.gameobjects;
 
-/**
- * Created by rodolfomatos on 30/05/16.
- */
-public class KeyToDirectionMapper {
+import org.academiadecodigo.simplegraphics.keyboard.KeyboardEvent;
 
+import java.util.EnumMap;
+import java.util.Map;
 
+public class KeyToDirectionMapper{
+
+    private static final Map<Integer, Direction> KeyToDirection = new EnumMap<Integer, Direction>(Direction.class);
+
+    static {
+
+        KeyToDirection.put(KeyboardEvent.KEY_RIGHT, Direction.EAST);
+        KeyToDirection.put(KeyboardEvent.KEY_LEFT, Direction.WEST);
+        KeyToDirection.put(KeyboardEvent.KEY_UP, Direction.NORTH);
+        KeyToDirection.put(KeyboardEvent.KEY_DOWN, Direction.SOUTH);
+
+    }
+
+    /**
+     * Get a Lanterna Terminal getColor from a grid getColor
+     *
+     * @param color the grid getColor
+     * @return the lanterna terminal getColor
+     */
+    public static Direction getDirection(KeyboardEvent kbEvent) {
+        return KeyToDirection.get(kbEvent.getKey());
+    }
 
 }
+
